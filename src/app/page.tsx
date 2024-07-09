e8c6b2bb1587a5f2d90f8935ef14acfefb679e61
Jayden Boesch <54635973+jaboesch@users.noreply.github.com>
2024-07-09 09:33:13 -0500

    Add dynamic import for synthesizer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Synthesizer from "@/components/synthesizer";
+import dynamic from 'next/dynamic';
+const Synthesizer = dynamic(() => import('@/components/synthesizer'), {
+  ssr: false,
+});
 
 export default function Home() {
-  if (typeof window === "undefined") return null;
   return (
     <main className="flex min-w-[1200px] min-h-screen flex-col items-center justify-between py-12">
       <Synthesizer />
